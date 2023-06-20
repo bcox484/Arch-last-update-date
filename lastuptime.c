5cd71cadca77021875e1a7c8ebe89e5b454de9d3
bcox484 <136201934+bcox484@users.noreply.github.com>
2023-06-20 16:31:10 -0600

    Add files via upload
    Fixed possible out of bounds pointer deference and infinite loop if update not found.

--- lastuptime.c
+++ lastuptime.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,8 +6,10 @@
 #define BUF_SIZE 1024
 #define END_CHAR 'T'
 #define FILENAME "/var/log/pacman.log"
+#define SEARCH_ITEM "full system upgrade"
 
-void lastUpgradeString(char *result, FILE *log, const char *update_command);
+void lastUpgradeString(char *result, FILE *log, const char *upgrade_string);
+void findStr(char *line, char *result, const char *upgrade_string);
 
 int main() {
   FILE *log = NULL;
@@ -17,7 +20,7 @@ int main() {
     return -1;
   }
 
-  lastUpgradeString(result, log, "full system upgrade");
+  lastUpgradeString(result, log, SEARCH_ITEM);
 
   fclose(log);
   if (result[0] == '\0') {
@@ -27,15 +30,22 @@ int main() {
 
   /* Copy memory address of result to date, increment through date */
   char *date = result;
-  for (date++; *date != END_CHAR; date++)
+  for (date++; *date != END_CHAR && *date != '\0'; date++)
     printf("%c", *date);
 
   printf("\n");
   return 0;
 }
 
+void findStr(char *line, char *result, const char *upgrade_string) {
+  if (strstr(line, upgrade_string)) {
+    strncpy(result, line, BUF_SIZE);
+    result[BUF_SIZE - 1] = '\0';
+  }
+}
+
 /* Search file bottom up in 1/5 chunks for last update in log */
-void lastUpgradeString(char *result, FILE *log, const char *update_command) {
+void lastUpgradeString(char *result, FILE *log, const char *upgrade_string) {
   char line[BUF_SIZE] = "";
   long size = 0;
 
@@ -46,24 +56,18 @@ void lastUpgradeString(char *result, FILE *log, const char *update_command) {
   /* Increment through first 20% of log file */
   float i = 0.8;
   fseek(log, (long)floor(size * i), SEEK_SET);
-  while (fgets(line, BUF_SIZE, log))
-    if (strstr(line, update_command)) {
-      strncpy(result, line, BUF_SIZE);
-    }
+  while (fgets(line, BUF_SIZE, log) != NULL)
+    findStr(line, result, upgrade_string);
 
-  /* Increment through file in 20% chunks, stop if 'update_command' is found */
-  for (i = 0.6; result[0] == '\0'; i -= 0.2) {
-    if (i == 0.0)
-      break;
+  /* Go through file in 20% chunks, stop if 'update_command' is found */
+  for (i = 0.6; result[0] == '\0' && i >= 0.0; i -= 0.2) {
     fseek(log, (long)floor(size * i), SEEK_SET);
-    long pos = (long)ceil(size * (i + 0.2));
+    long previous_pos = (long)ceil(size * (i + 0.2));
 
     /* Stop search if file position reaches beginning of last file position */
-    while (ftell(log) < pos + strlen(update_command)) {
-      fgets(line, BUF_SIZE, log);
-      if (strstr(line, update_command)) {
-        strncpy(result, line, BUF_SIZE);
-      }
+    while (ftell(log) <= (previous_pos + strlen(upgrade_string)) &&
+           fgets(line, BUF_SIZE, log) != NULL) {
+      findStr(line, result, upgrade_string);
     }
   }
 }
